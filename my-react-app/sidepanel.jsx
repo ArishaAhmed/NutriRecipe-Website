23d8f467c38cc02c71c72ae7bfd0017b18649d8e
Arisha Ahmed <148933778+ArishaAhmed@users.noreply.github.com>
2024-08-17 22:33:35 +0500

    Update sidepanel.jsx

--- my-react-app/sidepanel.jsx
+++ my-react-app/sidepanel.jsx
@@ -1,39 +1,3 @@
-// import React from 'react';
-// import './sidepanel.css';
-
-// const SidePanel = ({ selectedCategory, setSelectedCategory }) => {
-//   const handleClick = (category) => {
-//     setSelectedCategory(category);
-//   };
-
-//   return (
-//     <div className="side-panel">
-//       <button onClick={() => handleClick('BreakFast')}>
-//         Breakfast
-//       </button>
-//       <br />
-//       <button onClick={() => handleClick('Lunch')}>
-//         Lunch
-//       </button>
-//       <br />
-//       <button onClick={() => handleClick('Dinner')}>
-//         Dinner
-//       </button>
-//       <br />
-//       <button onClick={() => handleClick('Dessert')}>
-//         Dessert
-//       </button>
-//       <br />
-//       <button onClick={() => handleClick('Snack')}>
-//         Snack
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default SidePanel;
-
-
 import React from 'react';
 import './sidepanel.css';
 
